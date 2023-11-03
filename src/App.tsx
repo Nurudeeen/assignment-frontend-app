6baa3ad56f51e7f2907a785910bc078faa1b0ecf
Nurudeeen <nurudeenrabiu81@gmail.com>
2023-11-03 18:08:48 +0100

    feature: assignment frontend initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,75 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useRef } from "react";
+import { Button, Col, Container, Form, Navbar } from "react-bootstrap";
+import Row from 'react-bootstrap/Row';
+import { AuthContext } from "./context/AuthContext";
+import { auth } from "./firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const user = useContext(AuthContext);
+
+  const emailRef = useRef<HTMLInputElement>(null);
+  const passwordRef = useRef<HTMLInputElement>(null);
+
+
+const signIn = async () => {
+  try {
+    await auth.signInWithEmailAndPassword(
+      emailRef.current!.value,
+      passwordRef.current!.value
+    )
+    const user = auth.currentUser;
+    if (user) {
+      const idToken = await user.getIdToken();
+      console.log('idToken:', idToken);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const signOut = async () => {
+  await auth.signOut();
+};
+
+return (
+  <>
+    <Navbar className="justify-content-between" bg="dark" variant="dark">
+      <Navbar.Brand>Firebase Authentication</Navbar.Brand>
+      {user && <Button onClick={signOut}>Sign Out</Button>}
+    </Navbar>
+    {!user ? (
+      <Container style={{ maxWidth: "500px" }} fluid>
+        <Form className="mt-4">
+          <Form.Group controlId="formEmail">
+            <Form.Label>Email</Form.Label>
+            <Form.Control ref={emailRef} type="email" placeholder="email" />
+          </Form.Group>
+          <Form.Group controlId="formPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              ref={passwordRef}
+              type="password"
+              placeholder="password"
+            />
+          </Form.Group>
+          <Row>
+            <Col xs={6}>
+              <Button
+                onClick={signIn}
+                type="button"
+                variant="secondary"
+              >
+                Sign In
+              </Button>
+            </Col>
+          </Row>
+        </Form>
+      </Container>
+    ) : (
+      <h2 className="mt-4 text-center">Welcome {user.email}</h2>
+    )}
+  </>
+);
 }
 
 export default App;
